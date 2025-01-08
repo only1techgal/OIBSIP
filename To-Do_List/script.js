c03637a22dd0ffc017969f3048cf68cd2aacf7db
only1techgal <jenn.lipholo@gmail.com>
2025-01-08 15:28:05 +0200

    To-do list

---
+++ To-Do_List/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+   const taskInput = document.getElementById("task-input");
+   const addtaskBtn = document.getElementById("add-task-btn");
+   const pendingTasks = document.getElementById("pending-tasks");
+   const completedTasks = document.getElementById("completed-tasks");
+    
+}
